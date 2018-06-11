05b856430e696af6673f91e78c3b6b404152c0a5
Nate <Natmcnair2012@gmail.com>
2018-06-11 17:33:03 -0400

    Minor production refactoring

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<quiet-mountain-45123>.herokuapp.com'
+  host = 'quiet-mountain-45123.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
