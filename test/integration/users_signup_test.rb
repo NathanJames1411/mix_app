0e45a477eb4fb1062fd21e27a605f154059420a5
Nate <Natmcnair2012@gmail.com>
2018-06-11 17:28:00 -0400

    Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,8 +15,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
 
   test "valid signup information" do
@@ -24,8 +28,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
+    # assert_template 'users/show'
     assert_not flash.empty?
-    assert is_logged_in?
+    # assert is_logged_in?
   end
 end
